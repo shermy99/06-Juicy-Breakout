be0ac20214480a5030ed15ea92cca8a107d1f4ee
Chase <dragonfire1199@gmail.com>
2020-02-17 18:50:22 -0500

    makes tiles bounce randomly

--- Scripts/Tile.gd
+++ Scripts/Tile.gd
@@ -6,9 +6,10 @@ onready var _target = position
 onready var t = $Timer
 
 func _ready():
+	randomize()
 	position.y = -30
 	var time = rand_range(0,2.5)
-	t.set_wait_time(true)
+	t.set_wait_time(time)
 	t.start()
 	yield(t, "timeout")
 	$Tween.interpolate_property(self, "position", position, _target, 2.0, Tween.TRANS_BOUNCE, Tween.EASE_OUT)
